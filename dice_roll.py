7b69b1da222fff2905d819113732ea4f69c059b0
Miguel Johnson <miguelbjohnson@gmail.com>
2020-05-02 11:41:31 -0400

    Set up random dice spin to get total

--- dice_roll.py
+++ dice_roll.py
@@ -1,16 +1,27 @@
+from random import randint
+
 class Dice(object):
     def __init__(self, object):
         self.dice_name = object
         self.min_roll = 1
         self.max_roll = 6
     
-    def roll_dice(self, rolled = True):
-        self.rolled = rolled
+    def roll_dice(self):
+        return randint(self.min_roll, self.max_roll)
 
     
 dice_1 = Dice('dice_1')
 dice_2 = Dice('dice_2')
 
-print(dice_1.__dict__)
-print(dice_2.__dict__)
-print(dice_1.max_roll)
+total = dice_1.roll_dice() +  dice_2.roll_dice()
+print(total)
+
+# user = input(f'What is your name ')
+# exit_status = ''
+
+# while exit_status is not 'q':
+    
+#     print(total)
+#     exit_status = input(f'Press any key to continue or')
+
+
